9741941ee9134c981b17fd50019a5507fe7babca
Wayne Andrew <wayne.andrew@gmail.com>
2020-06-11 14:20:53 -0700

    Trying to account for fully negative slices broke everything -- this is stable

--- maxslice/maxslice.go
+++ maxslice/maxslice.go
@@ -1,8 +1,6 @@
 package maxslice
 
-import "fmt"
-
-// O(n2) /// GAH this actually doesn't quite work either since it can pull a single.
+// O(n2)
 func FindMax(s []int) int {
 	l, result := len(s), 0
 	for i := 0; i < l; i++ {
@@ -11,12 +9,8 @@ func FindMax(s []int) int {
 			//			fmt.Printf("%d %d %d\n", i, u, result)
 			u += s[n]
 			// has result be set yet?
-			if i < 2 {
-				result = u
-			} else {
-				result = Max(result, u)
-			}
 
+			result = Max(result, u)
 		}
 	}
 
@@ -25,20 +19,13 @@ func FindMax(s []int) int {
 
 // I HATE nested loops.... but I wanted to leave the original attempt (since that was what I was working toward)... I
 // think this is O(n)
-// this doesn't work yet for all - integers
 func FindMaxB(s []int) int {
 	var start, mEnd, mS int = 0, 0, 0
 
-	i := 0
 	for _, n := range s {
-		if i < 2 {
-			start = mEnd + n
-			fmt.Printf("%d %d %d\n", start, mEnd, n)
-		}
 		//		fmt.Printf("%d %d %d\n", n, mEnd, mS)
 		mEnd = Max(start, mEnd+n)
 		mS = Max(mS, mEnd)
-		i++
 	}
 
 	return mS
