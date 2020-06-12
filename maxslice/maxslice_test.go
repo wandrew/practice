4779b20027f2250013883d679d4f7f26a169344c
Wayne Andrew <wayne.andrew@gmail.com>
2020-06-12 15:08:01 -0700

    Clarity to test cases

--- maxslice/maxslice_test.go
+++ maxslice/maxslice_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestFindMax(t *testing.T) {
 	tests := map[int][]int{
-		9:  {-1, -4, 4, 4, -9, 10},             // this was just wrong... it WAS 9
-		14: {3, -4, 4, 4, -9, 7, 7, -22},       //.
-		-7: {-8, -9, -3, -4, -9, -13, -7, -22}, // fails because -3
+		9:  {-1, -4, 4, 4, -9, 10},             // this was just wrong... it WAS 9 [4,4,-9,10]
+		14: {3, -4, 4, 4, -9, 7, 7, -22},       // [7, 7]
+		-7: {-8, -9, -3, -4, -9, -13, -7, -22}, // [-3, -4]
 	}
 
 	for expected, test := range tests {
