da68ba63a22b826e86e51a8247312e7925413758
Wayne Andrew <wayne.andrew@gmail.com>
2020-06-12 14:45:53 -0700

    Getting closer

--- maxslice/maxslice.go
+++ maxslice/maxslice.go
@@ -8,27 +8,34 @@ type Solution int
 func FindMax(s []int) int {
 	var result int
 	l := len(s)
-	for i := 1; i < l-1; i++ {
+	for i := 0; i < l; i++ {
 		var u int /// I want the number to be negative
 		fmt.Printf("Iteration %d\n", i)
 
+		addends := 0
 		for n := i; n < l; n++ {
+			addends += 1
 			//			fmt.Printf("%d %d %d\n", i, u, result)
 			// Check if u is initialized
-			if n == 0 {
-				fmt.Printf("Var u is empty for index %d, skipping sum and assigning to %d...\n", n, s[n])
+			if n == i {
+				fmt.Printf("Var u is empty for index %+d, skipping sum and assigning to %+d...\n", n, s[n])
 				u = s[n]
 				continue
 			} else {
-				fmt.Printf("Summing %d + %d for index %d...\n", u, s[n], n)
+				fmt.Printf("Summing %+d + %+d for index %+d...\n", u, s[n], n)
 				u += s[n]
-				fmt.Printf("Sum is: %d\n", u)
+				fmt.Printf("Sum is: %+d\n", u)
 			}
 
-			fmt.Printf("Current Sum of U for index %d is: %d\n", n, u)
+			fmt.Printf("Current Sum of U for index %+d is: %+d\n", n, u)
 			// has result be set yet?
-			fmt.Printf("Evaluating greater of last result: %d and current computed result %d\n", result, u)
-			result = Max(result, u)
+			fmt.Printf("Evaluating greater of last result: %+d and current computed result %+d\n", result, u)
+			// we want to set the result only after we have the sum of 2 indexes and never when only a single index is included
+			// set our first result if we have 2 or more addends
+			if addends > 1 {
+				result = Max(result, u)
+			}
+
 		}
 	}
 
